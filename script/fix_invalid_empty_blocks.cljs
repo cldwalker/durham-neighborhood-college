93b46ad70b34577c2bc669a4ecf7a94d3c9e5ad4
Gabriel Horner <gabriel.horner@gmail.com>
2023-10-05 10:53:45 -0400

    Migrated to latest :default property implementation
    Also updated frontend as the frontend may be effected

---
+++ script/fix_invalid_empty_blocks.cljs
@@ -0,0 +1,42 @@
+(ns fix-invalid-empty-blocks
+  "This script deletes empty blocks that had no :block/left or :block/parent
+   attributes. This was useful to delete property values for multi-line :default properties
+   since they don't delete currently and the implementation changed"
+  (:require [logseq.tasks.db-graph.persist-graph :as persist-graph]
+            [logseq.db.sqlite.cli :as sqlite-cli]
+            [logseq.db.sqlite.db :as sqlite-db]
+            [datascript.core :as d]
+            [clojure.string :as string]
+            [nbb.core :as nbb]
+            ["path" :as node-path]
+            ["os" :as os]))
+
+(defn -main [args]
+  (when (empty? args)
+    (println "Usage: $0 GRAPH-DIR")
+    (js/process.exit 1))
+  (let [[graph-dir] args
+        dry-run? (contains? (set args) "-n")
+        [dir db-name] (if (string/includes? graph-dir "/")
+                        ((juxt node-path/dirname node-path/basename) graph-dir)
+                        [(node-path/join (os/homedir) "logseq" "graphs") graph-dir])
+        _ (sqlite-db/open-db! dir db-name)
+        conn (sqlite-cli/read-graph db-name)
+        query '[:find ?b
+                :where
+                [?b :block/content ""]
+                (or [(missing? $ ?b :block/left)]
+                    [(missing? $ ?b :block/parent)])]
+        blocks-to-update (map first (d/q query @conn))
+        update-tx (map #(vector :db.fn/retractEntity %)
+                          blocks-to-update)]
+    (if dry-run?
+      (do (println "Would update" (count blocks-to-update) "blocks with the following tx:")
+          (prn update-tx))
+      (do
+        (persist-graph/add-listener conn db-name)
+        (d/transact! conn update-tx)
+        (println "Updated" (count update-tx) "block(s) for graph" (str db-name "!"))))))
+
+(when (= nbb/*file* (:file (meta #'-main)))
+  (-main *command-line-args*))
