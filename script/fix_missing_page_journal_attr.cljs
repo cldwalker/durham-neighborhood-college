c02861d0a6db941630890b90b61daaeaf520eebd
Gabriel Horner <gabriel.horner@gmail.com>
2023-10-06 11:32:05 -0400

    fix pages with missing :block/journal?

---
+++ script/fix_missing_page_journal_attr.cljs
@@ -0,0 +1,40 @@
+(ns fix-missing-page-journal-attr
+  "This script fixes pages that don't have :block/journal?"
+  (:require [logseq.tasks.db-graph.persist-graph :as persist-graph]
+            [logseq.db.sqlite.cli :as sqlite-cli]
+            [logseq.db.sqlite.db :as sqlite-db]
+            [datascript.core :as d]
+            [clojure.string :as string]
+            [nbb.core :as nbb]
+            ["path" :as node-path]
+            ["os" :as os]))
+
+(defn -main [args]
+  (when (empty? args)
+    (println "Usage: $0 GRAPH-DIR")
+    (js/process.exit 1))
+  (let [[graph-dir] args
+        dry-run? (contains? (set args) "-n")
+        [dir db-name] (if (string/includes? graph-dir "/")
+                        ((juxt node-path/dirname node-path/basename) graph-dir)
+                        [(node-path/join (os/homedir) "logseq" "graphs") graph-dir])
+        _ (sqlite-db/open-db! dir db-name)
+        conn (sqlite-cli/read-graph db-name)
+        query '[:find ?b
+                :where
+                (or [?b :block/type "property"]
+                    [?b :block/type "class"])
+                [(missing? $ ?b :block/journal?)]]
+        blocks-to-update (map first (d/q query @conn))
+        update-tx (mapv #(hash-map :db/id % :block/journal? false)
+                        blocks-to-update)]
+    (if dry-run?
+      (do (println "Would update" (count blocks-to-update) "blocks with the following tx:")
+          (prn update-tx))
+      (do
+        (persist-graph/add-listener conn db-name)
+        (d/transact! conn update-tx)
+        (println "Updated" (count update-tx) "block(s) for graph" (str db-name "!"))))))
+
+(when (= nbb/*file* (:file (meta #'-main)))
+  (-main *command-line-args*))
