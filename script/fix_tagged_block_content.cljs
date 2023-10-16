92e7e7eb14982e9642d689fe40a2f3f7da04d4da
Gabriel Horner <gabriel.horner@gmail.com>
2023-10-16 08:08:07 -0400

    Strip tagged blocks of tags in content
    Also bump to latest frontend which has the change with block content
    and tags

---
+++ script/fix_tagged_block_content.cljs
@@ -0,0 +1,46 @@
+(ns fix-tagged-block-content
+  "This script fixes tagged blocks with :block/content that still has its tags in it.
+   We stopped including tags in content the week of Oct 9."
+  (:require [logseq.tasks.db-graph.persist-graph :as persist-graph]
+            [logseq.db.sqlite.cli :as sqlite-cli]
+            [logseq.db.sqlite.db :as sqlite-db]
+            [datascript.core :as d]
+            [clojure.string :as string]
+            [nbb.core :as nbb]
+            ["path" :as node-path]
+            ["os" :as os]))
+
+(defn -main [args]
+  (when (empty? args)
+    (println "Usage: $0 GRAPH-DIR")
+    (js/process.exit 1))
+  (let [[graph-dir] args
+        dry-run? (contains? (set args) "-n")
+        [dir db-name] (if (string/includes? graph-dir "/")
+                        ((juxt node-path/dirname node-path/basename) graph-dir)
+                        [(node-path/join (os/homedir) "logseq" "graphs") graph-dir])
+        _ (sqlite-db/open-db! dir db-name)
+        conn (sqlite-cli/read-graph db-name)
+        query '[:find (pull ?b [:db/id :block/content])
+                :where
+                [?b :block/tags]
+                [?b :block/content ?content]
+                [(clojure.string/includes? ?content "#")]
+                [(missing? $ ?b :block/link)]]
+        blocks-to-update (map first (d/q query @conn))
+        update-tx (mapv #(update % :block/content
+                                 (fn [content]
+                                  ;;  (println (pr-str content) "->" (pr-str (second (re-find #"(.*\S)\s+#\S+$" content))))
+                                   (or (second (re-find #"(.*\S)\s+#\S+$" content))
+                                       (throw (ex-info "Block doesn't have content to replace" {:content content})))))
+                        blocks-to-update)]
+    (if dry-run?
+      (do (println "Would update" (count blocks-to-update) "blocks with the following tx:")
+          (prn update-tx))
+      (do
+        (persist-graph/add-listener conn db-name)
+        (d/transact! conn update-tx)
+        (println "Updated" (count update-tx) "block(s) for graph" (str db-name "!"))))))
+
+(when (= nbb/*file* (:file (meta #'-main)))
+  (-main *command-line-args*))
