499a1eeda6631c0fd69a8c9b20f9887e718fb80b
Gabriel Horner <gabriel.horner@gmail.com>
2024-02-02 21:32:26 -0500

    Update script deps and fix script
    to use recent db version

--- script/fix_tagged_block_content.cljs
+++ script/fix_tagged_block_content.cljs
@@ -1,10 +1,10 @@
 (ns fix-tagged-block-content
   "This script fixes tagged blocks with :block/content that still has its tags in it.
-   We stopped including tags in content the week of Oct 9."
-  (:require [logseq.outliner.cli.persist-graph :as persist-graph]
-            [logseq.db.sqlite.cli :as sqlite-cli]
+   We stopped including tags in content for a 2nd time with https://github.com/logseq/logseq/commit/575624c650b2b7e919033a79aa5d14b97507d86f.
+   First time we stopped including tags in content the week of Oct 9."
+  (:require [datascript.core :as d]
+            [logseq.outliner.cli.pipeline :as cli-pipeline]
             [logseq.db.sqlite.db :as sqlite-db]
-            [datascript.core :as d]
             [clojure.string :as string]
             [nbb.core :as nbb]
             ["path" :as node-path]
@@ -19,8 +19,7 @@
         [dir db-name] (if (string/includes? graph-dir "/")
                         ((juxt node-path/dirname node-path/basename) graph-dir)
                         [(node-path/join (os/homedir) "logseq" "graphs") graph-dir])
-        _ (sqlite-db/open-db! dir db-name)
-        conn (sqlite-cli/read-graph db-name)
+        conn (sqlite-db/open-db! dir db-name)
         query '[:find (pull ?b [:db/id :block/content])
                 :where
                 [?b :block/tags]
@@ -30,7 +29,7 @@
         blocks-to-update (map first (d/q query @conn))
         update-tx (mapv #(update % :block/content
                                  (fn [content]
-                                  ;;  (println (pr-str content) "->" (pr-str (second (re-find #"(.*\S)\s+#\S+$" content))))
+                                   (println (pr-str content) "->" (pr-str (second (re-find #"(.*\S)\s+#\S+$" content))))
                                    (or (second (re-find #"(.*\S)\s+#\S+$" content))
                                        (throw (ex-info "Block doesn't have content to replace" {:content content})))))
                         blocks-to-update)]
@@ -38,7 +37,7 @@
       (do (println "Would update" (count blocks-to-update) "blocks with the following tx:")
           (prn update-tx))
       (do
-        (persist-graph/add-listener conn db-name)
+        (cli-pipeline/add-listener conn)
         (d/transact! conn update-tx)
         (println "Updated" (count update-tx) "block(s) for graph" (str db-name "!"))))))
 
